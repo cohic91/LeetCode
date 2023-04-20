993292da7c7937ce519127eb9f7e3a73a0225698
Hyunmin Lee <75608471+cohic91@users.noreply.github.com>
2023-04-20 22:33:49 +0900

    Time: 0 ms (100.00%), Space: 40.4 MB (35.66%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -11,17 +11,13 @@
 class Solution {
     public ListNode middleNode(ListNode head) {
         int nodeLength = 1;
-        int headVal = head.val;
-        
         ListNode tail = head;
         while (tail.next != null) {
             tail = tail.next;
             nodeLength += 1;
         }
-        int tailVal = tail.val;
         
         int middleIndex = nodeLength / 2 + 1;
-        
         ListNode middleNode = head;
         for(int i = 1; i < middleIndex; i++) {
             middleNode = middleNode.next;
